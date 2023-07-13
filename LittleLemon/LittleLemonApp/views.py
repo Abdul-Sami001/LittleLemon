1fe677b61ef935a51224321afbbeabab0cd71bd1
Abdul-Sami001 <abdulsami.kh001@gmail.com>
2023-07-13 21:31:27 +0500

    Added APIs for menu and booking tabe

--- LittleLemon/LittleLemonApp/views.py
+++ LittleLemon/LittleLemonApp/views.py
@@ -1,5 +1,24 @@
+from rest_framework import viewsets
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 # Create your views here.
-def index(request):
-    return render(request, 'index.html',{})
+
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
